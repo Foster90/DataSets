3a941445875c75d02430de782fef1557d0cbfbf9
Matthew Foster <Matthew.Foster@Civica.co.uk>
2020-09-19 13:46:22 +0100

    Update to data set LINQ query Field1 over 50, File import and export.

--- DataSets/Form1.Designer.cs
+++ DataSets/Form1.Designer.cs
@@ -32,23 +32,32 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(79, 166);
+            this.dataGridView1.Location = new System.Drawing.Point(15, 45);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(630, 387);
+            this.dataGridView1.Size = new System.Drawing.Size(550, 341);
             this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(562, 55);
+            this.button1.Location = new System.Drawing.Point(345, 398);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(147, 52);
+            this.button1.Size = new System.Drawing.Size(112, 37);
             this.button1.TabIndex = 1;
             this.button1.Text = "Refresh Data";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,33 +65,118 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(153, 55);
+            this.textBox1.Location = new System.Drawing.Point(94, 407);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(313, 20);
+            this.textBox1.Size = new System.Drawing.Size(245, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(76, 58);
+            this.label1.Location = new System.Drawing.Point(17, 410);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(71, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Search GUID";
             // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(577, 45);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(464, 112);
+            this.dataGridView2.TabIndex = 4;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1047, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 112);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Result Above 50";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(679, 179);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(506, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(584, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Top 50 Strings";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(463, 398);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 37);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Export To File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 33);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Data Set";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(20, 449);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(437, 178);
+            this.dataGridView3.TabIndex = 10;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(463, 449);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 42);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Import From File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(760, 586);
+            this.ClientSize = new System.Drawing.Size(1236, 642);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,6 +188,14 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- DataSets/Form1.cs
+++ DataSets/Form1.cs
@@ -8,48 +8,159 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace DataSets
 {
     public partial class Form1 : Form
 
-        
+
+
+
     {
         string sConnectionString = "Data Source=LT15784\\SQLEXPRESS;Initial Catalog=test_DB;Integrated Security=True";
         DataTable dtbl;
+       
+
+
+        static DataTable dbmethod(string sConnectionString, DataTable datatable, DataGridView datagrid)
+
+        {
+            using (SqlConnection sqlCon = new SqlConnection(sConnectionString))
+
+            {
+
+
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM TestTable", sqlCon);
+                datatable = new DataTable();
+                sqlDa.Fill(datatable);
+
+
+                datagrid.DataSource = datatable;
+
+                return datatable;
+
+            }
+
+           
+        }
+
+
+        static void dbmethod2(string sConnectionString, DataTable datatable)
+
+        {
+            using (SqlConnection sqlCon = new SqlConnection(sConnectionString))
+
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM TestTable", sqlCon);
+                sqlDa.Fill(datatable);
+                 
+            }
+            
+        }
+
+
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+
+        private void Form1_Load(object sender, EventArgs e)
         {
-                    
+            dbmethod(sConnectionString, dtbl, dataGridView1);
+
+
+            DataTable dtbl2 = new DataTable();
+            dbmethod2(sConnectionString, dtbl2);
+            var datarow = dtbl2.AsEnumerable().Where(x => x.Field<int>("Field1") > 50);
+            DataView view = datarow.AsDataView();
+
+            var bindingSource1 = new BindingSource();
+            bindingSource1.DataSource = view;
+
+            comboBox1.DataSource = bindingSource1.DataSource;
+            comboBox1.DisplayMember = "Field3";
+
         }
 
+           
         private void button1_Click(object sender, EventArgs e)
         {
-            using (SqlConnection sqlCon = new SqlConnection(sConnectionString))
-
-            {
-                sqlCon.Open();
-                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM TestTable",sqlCon);
-                dtbl = new DataTable();
-                sqlDa.Fill(dtbl);
-
-
-                dataGridView1.DataSource = dtbl;
-
-            }
+            dbmethod(sConnectionString, dtbl, dataGridView1);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+            
+            dtbl = dbmethod(sConnectionString, dtbl, dataGridView1);
             DataView DV = new DataView(dtbl);
             DV.RowFilter = string.Format("TestTableID = '{0}'", textBox1.Text);
             dataGridView1.DataSource = DV;
 
         }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            DataTable dtbl2  = new DataTable();
+            dbmethod2(sConnectionString, dtbl2);
+
+            var datarow = dtbl2.AsEnumerable().Where(x => x.Field<int>("Field1") > 50);
+            DataView view = datarow.AsDataView();
+
+
+            dataGridView2.DataSource = view;
+
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            var ds = dbmethod(sConnectionString, dtbl, dataGridView1);
+
+            string JSONresult = JsonConvert.SerializeObject(ds);
+
+            string path = @"C:\json\export.json";
+          
+
+            using (var tw = new StreamWriter(path, true))
+            {
+                tw.WriteLine(JSONresult.ToString());
+                
+            }
+
+            MessageBox.Show("Exported to " +  path);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+            string path = @"C:\json\export.json";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                var obj = JsonConvert.DeserializeObject<DataTable>(sr.ReadToEnd());
+                                
+                dataGridView3.DataSource = obj;
+            }
+
+
+        }
+        
+
     }
 }
